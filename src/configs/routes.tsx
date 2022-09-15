bcc9a0e3e1ea6ce459e9e6b385cde14769ddeb5b
Huy Truong <59696851+Hi1805@users.noreply.github.com>
2022-09-15 17:33:11 +0700

    5

--- src/configs/routes.tsx
+++ src/configs/routes.tsx
@@ -1,4 +1,5 @@
-import { Homepage, LoginPage } from '@/pages';
+import { LoginPage } from '@/pages';
+import Homepage from '@/pages/Homepage/Homepage';
 
 // * Define all routes fro websites
 export const ROUTES = {
@@ -9,8 +10,9 @@ const publicRoutes: RouteFinder[] = [
   {
     path: ROUTES.login,
     page: LoginPage,
+    title: 'Login',
   },
-  { path: ROUTES.home, page: Homepage },
+  { path: ROUTES.home, page: Homepage, title: 'Homepage' },
 ];
 
 const privateRoutes: RouteFinder[] = [];
