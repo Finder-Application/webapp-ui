1554153a8a8e247f87d7a887d54b57d9b091b017
Hi1805 <nguyen.hoang1@hitachivantara.com>
2022-11-09 21:42:25 +0700

    yarn lint fix

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,4 +2,3 @@ export { default as Homepage } from './Homepage/Homepage';
 export { default as LoginPage } from './LoginPage/LoginPage';
 export { default as SearchPage } from './ReSearchPage/ReSearchPage';
 export { default as ForgotPasswordPage } from './ForgotPasswordPage/ForgotPasswordPage';
-
